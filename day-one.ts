8515eef0a1c94f19d956be42c2467504a6ad90a8
Vishen Gounden <vishen.gounden@glacier.co.za>
2023-11-28 12:20:30 +0200

    initial commit with template for day 1

---
+++ day-one.ts
@@ -0,0 +1,7 @@
+export const runPartOne = (input: string[]): number => {
+    return 0;
+};
+
+export const runPartTwo = (input: string[]): number => {
+    return 0;
+};
---
+++ index.ts
@@ -0,0 +1,15 @@
+import { readFileSync } from "fs";
+
+const day = 'day-one';
+
+const dayModule = await import(`./${day}`);
+const runPartOne: (allLines: string[]) => number = dayModule.runPartOne;
+const runPartTwo: (allLines: string[]) => number = dayModule.runPartTwo;
+
+
+const contents = readFileSync(`${day}-input.txt`, 'utf-8');
+const allLines = contents.split('\n');
+
+console.log(runPartOne(allLines));
+
+console.log(runPartTwo(allLines));
