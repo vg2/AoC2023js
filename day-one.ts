7d5db2e27b824538d891899bcfacaf5353d891f8
Vishen Gounden <vgounden@gmail.com>
2023-12-02 20:02:30 +0000

    complete day 1

--- day-one.ts
+++ day-one.ts
@@ -1,7 +1,51 @@
 export const runPartOne = (input: string[]): number => {
-    return 0;
+    let total = 0;
+    for(let i = 0; i < input.length; i++) {
+        const line = input[i];
+        const numbers = line.match(/[0-9]/g);
+        if (numbers) {
+            const lineValue = numbers[0] + numbers[numbers.length - 1];
+            total += parseInt(lineValue); 
+        }
+    }
+
+    return total;
 };
 
 export const runPartTwo = (input: string[]): number => {
-    return 0;
+    const matchValues = [ '1', '2', '3', '4', '5', '6', '7', '8', '9', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    let total = 0;
+
+    for (let i = 0; i < input.length; i++) {
+        let firstIndexMatch = 99999999;
+        let firstMatch = null;
+        let lastIndexMatch = -1;
+        let lastMatch = null;
+        for (let j = 0; j < matchValues.length; j++) {
+            let firstIndex = input[i].indexOf(matchValues[j]);
+            if (firstIndex !== -1 && firstIndex < firstIndexMatch) {
+                firstIndexMatch = firstIndex;
+                if (j < 9) {
+                    firstMatch = j + 1;
+                } else {
+                    firstMatch = j - 8;
+                }
+            }
+
+            let lastIndex = input[i].lastIndexOf(matchValues[j]);
+            if (lastIndex !== -1 && lastIndex > lastIndexMatch) {
+                lastIndexMatch = lastIndex;
+                if (j < 9) {
+                    lastMatch = j + 1;
+                } else {
+                    lastMatch = j - 8;
+                }
+            }
+        }
+        console.log('firstMatch:' + firstMatch);
+        console.log('lastMatch:' + lastMatch);
+        const lineValue = firstMatch?.toString() + lastMatch?.toString();
+        total += parseInt(lineValue);
+    }
+    return total;
 };
